c9cb38c7ea66b8f672217fb63e18048ae17f8e6a
Serega S <serega1067@yandex.ru>
2023-08-13 01:41:05 +0700

    Task homework 5 done

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <img :src="product.image" />
+        <p>name: {{ product.name }}</p>
+        <p>price: {{ formattedPrice }}</p>
+        <p v-if="product.available">Available</p>
+        <p v-else>Out of stock</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductDetails",
+
+    data() {
+        return {
+            product:
+            {
+                image: require("../assets/image.webp"),
+                name: 'Ubik',
+                price: 99.99,
+                available: false,
+            },
+        };
+    },
+
+    computed: {
+        formattedPrice: function () {
+            return `$${this.product.price}`;
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+    width: 25%;
+}
+
+div {
+    display: inline;
+    font-size: 24px;
+}
+</style>
